7f8f008f7487732d4892d64298f8daa2856c6ff2
John Capobianco <john.capobianco@parl.gc.ca>
2021-03-23 00:48:38 -0400

    j2 collapse

--- JUNOS_magic_carpet.py
+++ JUNOS_magic_carpet.py
@@ -14,32 +14,21 @@ from rich import print
 from rich.panel import Panel
 from rich.text import Text
 from pyats import aetest
-
-
-# ----------------
-# Jinja2
-# ----------------
 from jinja2 import Environment, FileSystemLoader
 
 template_dir = "templates/juniper"
 env = Environment(loader=FileSystemLoader(template_dir))
 
 # ----------------
-# Import pyATS and the pyATS Library
-# ----------------
-
 # Get logger for script
+# ----------------
 log = logging.getLogger(__name__)
 
 # ----------------
-# Template sources
+# Filetypes 
 # ----------------
 
-# show system_information
-sh_system_information_csv_template = env.get_template("show_system_information_csv.j2")
-sh_system_information_md_template = env.get_template("show_system_information_md.j2")
-sh_system_information_html_template = env.get_template("show_system_information_html.j2")
-
+filetype_loop = ["csv","md","html"]
 
 class common_setup(aetest.CommonSetup):
     """Common Setup section"""
@@ -47,7 +36,7 @@ class common_setup(aetest.CommonSetup):
     @aetest.subsection
     def connect_to_devices(self, testbed):
         """Connect to all the devices"""
-        print(Panel.fit(Text.from_markup("Hang on tight - we are about to go on a [bold blue]Magic[/bold blue] [bold yellow]Carpet[/bold yellow] ride!\n\n[purple].-.\n[.-''-.,\n|  //`~\)\n(<|[/][blue]0[/][purple]|>[/][blue]0[/][purple])\n;\  _/ \\_ _\,\n__\|'._/_  \ '='-,\n/\ \    || )_///_\>>\n(  '._ T |\ | _/),-'\n'.   '._.-' /'/ |\n| '._   _.'`-.._/\n,\ / '-' |/\n[_/\-----j\n_.--.__[_.--'_\__\n/         `--'    '---._\n/ '---.  -'. .'  _.--   '.\n\_      '--.___ _;.-o     /\n'.__ ___/______.__8----'\nc-'----'[/]\n\n", justify="center")))
+        print(Panel.fit(Text.from_markup("Hang on tight - we are about to go on a [blink][bold blue]Magic[/bold blue][/blink] [blink][bold yellow]Carpet[/bold yellow][/blink] ride!\n\n[purple].-.\n[.-''-.,\n|  //`~\)\n(<|[/][blue]0[/][purple]|>[/][blue]0[/][purple])\n;\  _/ \\_ _\,\n__\|'._/_  \ '='-,\n/\ \    || )_///_\>>\n(  '._ T |\ | _/),-'\n'.   '._.-' /'/ |\n| '._   _.'`-.._/\n,\ / '-' |/\n[_/\-----j\n_.--.__[_.--'_\__\n/         `--'    '---._\n/ '---.  -'. .'  _.--   '.\n\_      '--.___ _;.-o     /\n'.__ ___/______.__8----'\nc-'----'[/]\n\n",justify="center")))
         testbed.connect()
 
 
@@ -62,7 +51,7 @@ class CollectInformation(aetest.Testcase):
         # Loop over devices
         # ---------------------------------------
         for device in testbed:
-            print(Panel.fit(Text.from_markup("[blue]_.---.__\n.'        `-.\n/      .--.   |\n\/  / /    |_/\n`\/|/    _(_)\n___  /|_.--'    `.   .\n\  `--' .---.     \ /|\n)   `       \     //|\n| __    __   |   '/||\n|/  \  /  \      / ||\n||  |  |   \     \  |\n\|  |  |   /        |\n__\\@/  |@ | ___ \--'\n(     /' `--'  __)|\n__>   (  .  .--' & \n/   `--|_/--'     &  |\n|                 #. |\n|                 q# |\n\              ,ad#'\n`.________.ad####'\n`#####''''''\n`&#\n&# #&\n'#ba'\n'[/]\n\nThe [bold blue]Magic[/] [bold yellow]Carpet[/] is heading into the Code of Wonders\nGenie Parsing Has Begun", justify="center",)))
+            print(Panel.fit(Text.from_markup("[blue]_.---.__\n.'        `-.\n/      .--.   |\n\/  / /    |_/\n`\/|/    _(_)\n___  /|_.--'    `.   .\n\  `--' .---.     \ /|\n)   `       \     //|\n| __    __   |   '/||\n|/  \  /  \      / ||\n||  |  |   \     \  |\n\|  |  |   /        |\n__\\@/  |@ | ___ \--'\n(     /' `--'  __)|\n__>   (  .  .--' & \n/   `--|_/--'     &  |\n|                 #. |\n|                 q# |\n\              ,ad#'\n`.________.ad####'\n`#####''''''\n`&#\n&# #&\n'#ba'\n'[/]\n\nThe [blink][bold blue]Magic[/bold blue][/blink] [blink][bold yellow]Carpet[/][/blink] is heading into the [bold cyan]Cave of Wonders[/bold cyan]\n\n[bold blue]Genie[/bold blue] Parsing Has Begun",justify="center")))
 
             with steps.start("Parsing show system information", continue_=True) as step:
                 try:
@@ -74,30 +63,19 @@ class CollectInformation(aetest.Testcase):
 
                 # Show system information
                 if hasattr(self, "parsed_system_information"):
-                    output_from_parsed_system_information_csv_template = (sh_system_information_csv_template.render(variable=self.parsed_system_information["system-information"]))
-                    output_from_parsed_system_information_md_template = (sh_system_information_md_template.render(variable=self.parsed_system_information["system-information"]))
-                    output_from_parsed_system_information_html_template = (sh_system_information_html_template.render(variable=self.parsed_system_information["system-information"]))
+                    sh_system_information_csv_template = env.get_template("show_system_information_csv.j2")
 
-            # ---------------------------------------
-            # Create Files
-            # ---------------------------------------
-
-            # Show system information
-            if hasattr(self, "parsed_system_information"):
                 with open(f"Cave_of_Wonders/Juniper/Show_System_Information/{device.alias}_show_system_information.json", "w") as fid:
                     json.dump(self.parsed_system_information, fid, indent=4, sort_keys=True)
 
                 with open(f"Cave_of_Wonders/Juniper/Show_System_Information/{device.alias}_show_system_information.yaml", "w") as yml:
                     yaml.dump(self.parsed_system_information, yml, allow_unicode=True)
 
-                with open(f"Cave_of_Wonders/Juniper/Show_System_Information/{device.alias}_show_system_information.csv", "w") as fh:
-                    fh.write(output_from_parsed_system_information_csv_template)
+                for filetype in filetype_loop:
+                    parsed_output_type = (sh_system_information_csv_template.render(variable=self.parsed_system_information["system-information"],filetype_loop_jinja2=filetype))
 
-                with open(f"Cave_of_Wonders/Juniper/Show_System_Information/{device.alias}_show_system_information.md", "w") as fh:
-                    fh.write(output_from_parsed_system_information_md_template)
-
-                with open(f"Cave_of_Wonders/Juniper/Show_System_Information/{device.alias}_show_system_information.html","w") as fh:
-                    fh.write(output_from_parsed_system_information_html_template)
+                    with open(f"Cave_of_Wonders/Juniper/Show_System_Information/{device.alias}_show_system_information.csv", "w") as fh:
+                        fh.write(parsed_output_type)
 
         # Goodbye Banner
-        print(Panel.fit(Text.from_markup("You've made it out of the Code of Wonders on your [bold blue]Magic[/] [bold yellow]Carpet[/]!\nWhat treasures did you get?\n\n[bold yellow]_oOoOoOo_[/]\n([bold yellow]oOoOoOoOo[/])\n)`#####`(\n/         \ \n|  NETWORK  |\n|  D A T A  |\n\           /\n`=========`\n\nTo see the results of your Magic Carpet ride type\n pyats logs view\n\nWritten by John Capobianco March 2021", justify="center")))
+        print(Panel.fit(Text.from_markup("You've made it out of the [bold orange]Cave of Wonders[/bold orange] on your [blink][bold blue]Magic[/bold blue][/blink] [blink][bold yellow]Carpet[/][/blink]!\n[green]What treasures did you get?[/green]\n\n[bold yellow]_[blink]o[/blink]O[blink]o[/blink]O[blink]o[/blink]O[blink]o[/blink]_[/]\n([bold yellow][blink]o[/blink]O[blink]o[/blink]O[blink]o[/blink]O[blink]o[/blink]O[blink]o[/blink][/])\n)`#####`(\n/         \ \n|  [bold green]NETWORK[/bold green]  |\n|  [bold green]D A T A[/bold green]  |\n\           /\n`=========`\n\n\n\nYour Network Data can be found in the [bold cyan]Cave of Wonders[/bold cyan]\n\nType cd [bold cyan]Cave_of_Wonders[/bold cyan]\n\nTo see the log of your [blink][bold blue]Magic[/bold blue][/blink] [blink][bold yellow]Carpet[/][/blink] ride \nType [bold red]pyats logs view[/bold red]\n\nWritten by John Capobianco March 2021",justify="center")))

48f94c6aabbd24ac80b75c8eb14696164bd8c206
Renato Almeida de Oliveira <renato.almeida.oliveira@gmail.com>
2021-03-26 23:29:45 -0300

    Refactor IOS_XE_magic_carpet to dynamically load template and execute commands

---
+++ ascii_art.py
@@ -0,0 +1,51 @@
+GREETING = '''Hang on tight - we are about to go on a [blink][bold blue]Magic[/bold blue][/blink] [blink][bold yellow]Carpet[/bold yellow][/blink] ride!
+
+[purple].-.
+[.-''-.,
+|  //`~\)
+(<|[/][blue]0[/][purple]|>[/][blue]0[/][purple])
+;\  _/ \\_ _\,
+__\|'._/_  \ '='-,
+/\ \    || )_///_\>>
+(  '._ T |\ | _/),-'
+'.   '._.-' /'/ |
+| '._   _.'`-.._/
+,\ / '-' |/
+[_/\-----j
+_.--.__[_.--'_\__
+/         `--'    '---._
+/ '---.  -'. .'  _.--   '.
+\_      '--.___ _;.-o     /
+'.__ ___/______.__8----'
+c-'----'[/]
+
+'''
+RUNNING= '''[blue]_.---.__
+.'        `-.
+/      .--.   |
+\/  / /    |_/
+`\/|/    _(_)
+___  /|_.--'    `.   .
+\  `--' .---.     \ /|
+)   `       \     //|
+| __    __   |   '/||
+|/  \  /  \      / ||
+||  |  |   \     \  |
+\|  |  |   /        |
+__\\@/  |@ | ___ \--'
+(     /' `--'  __)|
+__>   (  .  .--' & 
+/   `--|_/--'     &  |
+|                 #. |
+|                 q# |
+\              ,ad#'
+`.________.ad####'
+`#####''''''
+`&#
+&# #&
+'#ba'
+'[/]
+
+The [blink][bold blue]Magic[/bold blue][/blink] [blink][bold yellow]Carpet[/][/blink] is heading into the [bold cyan]Cave of Wonders[/bold cyan]
+
+[bold blue]Genie[/bold blue] Parsing Has Begun'''
